4e411ad1b2f5f91ba5c86d587565b4fc58c4ff13
Meggan <meggtriplett@gmail.com>
2018-10-25 22:10:17 -0700

    updated app.js added question 7 and totalScore counter. updated index.html with layout look

--- app.js
+++ app.js
@@ -1,197 +1,109 @@
-// "use strict";
+"use strict";
 
-// // username welcome question
-// var userName = prompt("Please enter your name.");
-// alert("Hi, " + userName + ". I'm Meggan, thank you for visiting my portfolio." );
+// username welcome question
+var userName = prompt("Please enter your name.");
+alert("Hi, " + userName + ". I'm Meggan, thank you for visiting my portfolio." );
 
-// alert("Please respond to the following questions with Y/y/yes/YES or N/n/NO/no...");
+alert("Please respond to the following questions with Y/y/yes/YES or N/n/NO/no...");
 
-// //asking question one and directing alert response based off the yes/no answer
-// var responseOne = prompt ("Do I like spicy food?") .toUpperCase();
-// if (responseOne === "Y" || responseOne === "YES") {
-//   alert ("Correct! I love spicy food.")
-//   } else if (responseOne === "N" || responseOne === "NO") {
-//   alert ("Nope. I actually love spicy food.")
-// };
-// // console log for to show question with user answer
-// console.log("Question 1: Do I like spicy food? User answer: ", responseOne);
+var totalScore = 0;
 
-// //asking question two and directing alert response based off the yes/no answer
-// var responseTwo = prompt ("Do I like country music?") .toUpperCase();
-// if (responseTwo === "N" || responseTwo === "NO") {
-//   alert ("Correct! I loathe country music. Sorry if you like it.")
-//   } else if (responseTwo === "Y" || responseTwo === "YES") {
-//   alert ("Sorry, I don't like country music.")
-// };
-// // console log for to show question with user answer
-// console.log("Question 2: Do I like country music? User answer: ", responseTwo);
+//asking question one and directing alert response based off the yes/no answer
+var responseOne = prompt ("Do I like spicy food?") .toUpperCase();
+if (responseOne === "Y" || responseOne === "YES") {
+  alert ("Correct! I love spicy food.");
+  totalScore++;
+  } else if (responseOne === "N" || responseOne === "NO") {
+  alert ("Nope. I actually love spicy food.")
+};
+// console log for to show question with user answer
+console.log("Question 1: Do I like spicy food? User answer: ", responseOne);
 
-// //asking question three and directing alert response based off the yes/no answer
-// var responseThree = prompt ("Have I met Jeff Bezos?") .toUpperCase();
-// if (responseThree === "Y" || responseThree === "YES") {
-//   alert ("You are right. I met him at a reading event at the EMP, now MOPOP, while I was in elementary.")
-//   } else if (responseThree === "N" || responseThree === "NO") {
-//   alert ("Surprising I actually met him at a reading event at the EMP, now MOPOP, while I was in elementary.")
-// };
-// // console log for to show question with user answer
-// console.log("Question 3: Have I met Jeff Bezos? User answer: ", responseThree);
+//asking question two and directing alert response based off the yes/no answer
+var responseTwo = prompt ("Do I like country music?") .toUpperCase();
+if (responseTwo === "N" || responseTwo === "NO") {
+  alert ("Correct! I loathe country music. Sorry if you like it.");
+  totalScore++;
+  } else if (responseTwo === "Y" || responseTwo === "YES") {
+  alert ("Sorry, I don't like country music.")
+};
+// console log for to show question with user answer
+console.log("Question 2: Do I like country music? User answer: ", responseTwo);
 
-// //asking question four and directing alert response based off the yes/no answer
-// var responseFour = prompt ("Was I born in America?") .toUpperCase();
-// if (responseFour === "N" || responseFour === "NO") {
-//   alert ("You're right. I was born in Kaohsiung, Taiwan.")
-//   } else if (responseFour === "Y" || responseFour === "YES") {
-//   alert ("Actually I was born in Kaohsiung, Taiwan.")
-// };
-// // console log for to show question with user answer
-// console.log("Question 4: Was I born in America? User answer: ", responseFour);
+//asking question three and directing alert response based off the yes/no answer
+var responseThree = prompt ("Have I met Jeff Bezos?") .toUpperCase();
+if (responseThree === "Y" || responseThree === "YES") {
+  alert ("You are right. I met him at a reading event at the EMP, now MOPOP, while I was in elementary.");
+  totalScore++;
+  } else if (responseThree === "N" || responseThree === "NO") {
+  alert ("Surprising I actually met him at a reading event at the EMP, now MOPOP, while I was in elementary.")
+};
+// console log for to show question with user answer
+console.log("Question 3: Have I met Jeff Bezos? User answer: ", responseThree);
 
-// //asking question five and directing alert response based off the yes/no answer
-// var responseFive = prompt ("Do I have a dog?") .toUpperCase();
-// if (responseFive === "Y" || responseFive === "YES") {
-//   alert ("I do have a dog. In fact I have two!")
-//   } else if (responseFive === "N" || responseFive === "NO") {
-//   alert ("You are kind of correct. I don't have a dog, I have two.")
-// };
-// // console log for to show question with user answer
-// console.log("Question 5: Do I have a dog? User answer: ", responseFive);
+//asking question four and directing alert response based off the yes/no answer
+var responseFour = prompt ("Was I born in America?") .toUpperCase();
+if (responseFour === "N" || responseFour === "NO") {
+  alert ("You're right. I was born in Kaohsiung, Taiwan.");
+  totalScore++;
+  } else if (responseFour === "Y" || responseFour === "YES") {
+  alert ("Actually I was born in Kaohsiung, Taiwan.")
+};
+// console log for to show question with user answer
+console.log("Question 4: Was I born in America? User answer: ", responseFour);
 
-// //asking question 6 and directing user to too high or too low prompts, user allowed four answer attempts
-// var responseSix = parseInt (prompt ("Can you guess how many times I have been to Disneyland?"));
-// var question6Answer = 2; //if six is broken change this
-// var counterQ6 = 0;
+//asking question five and directing alert response based off the yes/no answer
+var responseFive = prompt ("Do I have a dog?") .toUpperCase();
+if (responseFive === "Y" || responseFive === "YES") {
+  alert ("I do have a dog. In fact I have two!");
+  totalScore++;
+  } else if (responseFive === "N" || responseFive === "NO") {
+  alert ("You are kind of correct. I don't have a dog, I have two.")
+};
+// console log for to show question with user answer
+console.log("Question 5: Do I have a dog? User answer: ", responseFive);
 
-// //create counter in while loop so user can only attempt answer four times
-// while (responseSix !== 2 && counterQ6 < 3) {
-//   counterQ6++;
-//   if (responseSix > 2) {
-//     responseSix = parseInt (prompt("That is actually more times then I've gone. Guess again."));
-//   } else {
-//     responseSix = parseInt (prompt("I've actually been there more times. Try guessing again."));
-//   }
-// }
-// //correct answer for question 6 and alert for user to read
-// if (question6Answer === 2 ) {
-//   alert ("That is correct! Good guess.")
-// }
+//asking question six and directing user to too high or too low prompts, user allowed four answer attempts
+var responseSix = parseInt (prompt ("Can you guess how many times I have been to Disneyland?"));
+var question6Answer = 2; //if six is broken change this
+var counterQ6 = 0;
 
-// //asking question 7, answers and limiting the user attempts to six
-// var question7Answers = ["CANADA", "TAIWAN", "JAPAN", "THAILAND"];
-// var counterQ7 = 0;
-// var arrayID = 0;
-
-// var responseSeven = prompt ("Besides America, can you name a country I've traveled to?") .toUpperCase();
-
-// while (responseSeven !== question7Answers.arrayID && arrayID > 3 && counterQ7 < 6) {
-//   arrayID++;
-//   if (responseSeven > 3) {
-//     responseSeven = (prompt ("Nope. You only have 6 attempts in total. So try again.") .toUpperCase());
-//   } else {
-//     responseSeven = (prompt ("That is correct! I've been to Canada, Taiwan, Japan and Thailand.") .toUpperCase());
-//   } 
-// }
-
-
-
-
-
-
-function question1(){
-  var responseOne = prompt ('Do I like spicy food?') .toUpperCase();
-  if (responseOne === 'Y' || responseOne === 'YES') {
-    alert ('Correct! I love spicy food.');
-  } else  {
-    alert ('Nope. I actually love spicy food.');
+//create counter in while loop so user can only attempt answer four times
+while (responseSix !== 2 && counterQ6 < 3) {
+  counterQ6++;
+  if (responseSix > 2) {
+    responseSix = parseInt (prompt("That is actually more times then I've gone. Guess again."));
+  } else {
+    responseSix = parseInt (prompt("I've actually been there more times. Try guessing again."));
   }
-  console.log('Question 1: Do I like spicy food? User answer: ', responseOne);
 }
-question1();
-  
-  
-  
-function quesion2(){
-  
-  var responseTwo = prompt("Do I like country music?").toUpperCase();
-  if (responseTwo=== "Y" || responseTwo === "YES") {
-  
-    alert ("Correct! I love spicy food.");
-  } else  {
-    alert("Nope. I actually love spicy food.");
-  }
-  console.log('Question 2: Do I like country music? User answer: ', responseTwo);
-  
+//correct answer for question 6 and alert for user to read
+if (question6Answer === 2 ) {
+  alert ("That is correct! I went once at the age of 7 and more recently at 26.");
+  totalScore++;
 }
-quesion2();
-  
-  
-function question3(){
-  
-  var responseThree = prompt ('Have I met Jeff Bezos?') .toUpperCase();
-  if (responseThree === 'Y' || responseThree === 'YES') {
-    alert ('You are right. I met him at a reading event at the EMP, now MOPOP, while I was in elementary.');
-  } else if (responseThree === 'N' || responseThree === 'NO') {
-    alert ('Surprising I actually met him at a reading event at the EMP, now MOPOP, while I was in elementary.');
-  }
-  // console log for to show question with user answer
-  console.log('Question 3: Have I met Jeff Bezos? User answer: ', responseThree);
-  
-  
-}
-  
-question3();
-  
-function question4(){
-  
-  var responseThree = prompt ('Have I met Jeff Bezos?') .toUpperCase();
-  if (responseThree === 'Y' || responseThree === 'YES') {
-    alert ('You are right. I met him at a reading event at the EMP, now MOPOP, while I was in elementary.');
-  } else if (responseThree === 'N' || responseThree === 'NO') {
-    alert ('Surprising I actually met him at a reading event at the EMP, now MOPOP, while I was in elementary.');
-  }
-  // console log for to show question with user answer
-  console.log('Question 3: Have I met Jeff Bezos? User answer: ', responseThree);
-  
-}
-question4();
-  
-  
-function qeustion5(){
-  var responseFive = prompt ('Do I have a dog?') .toUpperCase();
-  if (responseFive === 'Y' || responseFive === 'YES') {
-    alert ('I do have a dog. In fact I have two!');
-  } else if (responseFive === 'N' || responseFive === 'NO') {
-    alert ('You are kind of correct. I don\'t have a dog, I have two.');
-  }
-  // console log for to show question with user answer
-  console.log('Question 5: Do I have a dog? User answer: ', responseFive);
-  
-}
-qeustion5();
-  
-  
-  
-  
-function question6(){
-  
-  
-  var responseSix = parseInt (prompt ('Can you guess how many times I have been to Disneyland?'));
-  var question6Answer = 2; //if six is broken change this
-  var counterQ6 = 0;
-    
-  //create counter in while loop so user can only attempt answer four times
-  while (responseSix !== 2 && counterQ6 < 3) {
-    counterQ6++;
-    if (responseSix > 2) {
-      responseSix = parseInt (prompt('That is actually more times then I\'ve gone. Guess again.'));
-    } else {
-      responseSix = parseInt (prompt('I\'ve actually been there more times. Try guessing again.'));
+
+//asking question seven and comparing user answer to array, if correct break loop, if incorrect only allow six attempts
+var question7Answers = ["CANADA", "TAIWAN", "JAPAN", "THAILAND"];
+var responseSeven = prompt ("Besides America, can you name a country I've traveled to?") .toUpperCase();
+var answerStatus = false;
+var counterQ7 = 0;
+
+while (counterQ7 < 5 && answerStatus === false) {
+  for (var arrayID = 0; arrayID < question7Answers.length; arrayID++) {
+    if (responseSeven === question7Answers[arrayID]) {
+      answerStatus = true;
+      break;
     }
   }
-  //correct answer for question 6 and alert for user to read
-  if (question6Answer === 2 ) {
-    alert ('That is correct! Good guess.');
+    if (answerStatus === true) {
+      alert("That is correct! I've been to Canada, Taiwan, Japan and Thailand." );
+      totalScore++;
+    } else {
+      responseSeven = prompt ("That is incorrect. Please try again.");
+      counterQ7++;
+    }
   }
-    
-  
-}
-question6();
+
+//user score message
+alert (userName + ", you scored " + totalScore + " out of 7 questions." )
